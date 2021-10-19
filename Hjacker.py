bede5e7d9d035526ab83310e21621ccce4b66ed0
ToxidWorm <79449659+toxidworm@users.noreply.github.com>
2021-10-20 00:00:46 +0300

    Update Hjacker V2

--- Hjacker.py
+++ Hjacker.py
@@ -1,24 +1,34 @@
-from colorama import Fore, Style
-from colorama import init
+from colorama import Fore, Style, init
 import platform as pf
-import requests
-import os
-import getpass
 import pyautogui as pg
-import base64
+import sys, time, threading, itertools, random, base64, getpass, os, requests, random
 init()
+done = False
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write(f'\r{Fore.GREEN}Loading Hjacker ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rHjacker Sucefully loaded')
 
+t = threading.Thread(target=animate)
+t.start()
+
+#long process here
+time.sleep(2)
+done = True
 os.system('cls')
 print(f"""{Fore.MAGENTA}
- ██░ ██ ▄▄▄██▀▀▀▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███  
-▓██░ ██▒  ▒██  ▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
-▒██▀▀██░  ░██  ▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
-░▓█ ░██▓██▄██▓ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
-░▓█▒░██▓▓███▒   ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
- ▒ ░░▒░▒▒▓▒▒░   ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
- ▒ ░▒░ ░▒ ░▒░    ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
- ░  ░░ ░░ ░ ░    ░   ▒   ░        ░ ░░ ░    ░     ░░   ░ 
- ░  ░  ░░   ░        ░  ░░ ░      ░  ░      ░  ░   ░     
+ ▄▄   ▄▄     ▄▄▄ ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄   ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   
+█  █ █  █   █   █      █       █   █ █ █       █   ▄  █  
+█  █▄█  █   █   █  ▄   █       █   █▄█ █    ▄▄▄█  █ █ █  
+█       █▄  █   █ █▄█  █     ▄▄█      ▄█   █▄▄▄█   █▄▄█▄ 
+█   ▄   █ █▄█   █      █    █  █     █▄█    ▄▄▄█    ▄▄  █
+█  █ █  █       █  ▄   █    █▄▄█    ▄  █   █▄▄▄█   █  █ █
+█▄▄█ █▄▄█▄▄▄▄▄▄▄█▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄█ █▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█  
                                     Coded by ToxidWorm
                                     Type 'help' for help{Fore.RESET}""")
 print(Fore.MAGENTA + 'Computer name: ' + f'{pf.node()}')
@@ -32,7 +42,7 @@ while True:
         print(f"IP Adress: {response['ip']}")
     elif command == 'help':
         print(' ')
-        print('getip - Get your current ip adress\nclear - clear screen\nexit - Exit\nscreenshot - Make screenshot\nflood - SMS & Call flood\nbase64 - Encode string (base64 algorithm)')
+        print('getip - Get your current ip adress\nclear - clear screen\nexit - Exit\nscreenshot - Make screenshot\nflood - SMS & Call flood\nbase64encode - Encode string (base64 algorithm)\nbase64decode - Decode string (base64 algorithm)\nutillist - Show utility list')
         print(' ')
     elif command == 'exit':
         exit()
@@ -49,8 +59,10 @@ while True:
             print(Fore.RED + 'You canceled attack')
             print(' ')
         else:
-            os.system('python Impulse/impulse.py --method SMS --time 20 --threads 15 --target +' + number)
-    elif command == 'base64':
+            attackthreads = input('Enter threads count: ')
+            attacktime = input('Enter attack time: ')
+            os.system('python Impulse/impulse.py --method SMS --time ' + attacktime + ' --threads ' + attackthreads + ' --target +' + number)
+    elif command == 'base64encode':
         sample_string = input('Enter string to encode: ')
         sample_string_bytes = sample_string.encode("ascii")
   
@@ -58,6 +70,13 @@ while True:
         base64_string = base64_bytes.decode("ascii")
 
         print(f"Encoded string: {base64_string}")
+    elif command == 'base64decode':
+        encodedstring = input('Enter encoded string: ')
+        encodedresult = base64.b64decode(encodedstring)
+        print(Fore.GREEN + 'Decoded string: ' + f'{encodedresult}')
+    elif command == 'utillist':
+        os.system('cls')
+        print(f'{Fore.MAGENTA}Tool list:\nImpulse by LimerBoy (https://github.com/LimerBoy/Impulse)')
     else:
         print(' ')
         print(Fore.RED + 'Unknown command')
